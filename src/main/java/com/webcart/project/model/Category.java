d56a4ecc3ef4105334fa4b31cc5834ee8739e265
wenbojiang <jiangwb70@yahoo.ca>
2024-09-14 12:08:21 -0500

    Added the lombok

--- src/main/java/com/webcart/project/model/Category.java
+++ src/main/java/com/webcart/project/model/Category.java
@@ -4,37 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Category {
-    public void setCategoryId(long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public Category(long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category(){}
-
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long categoryId;
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public long getCategoryId() {
-        return categoryId;
-    }
-
     private String categoryName;
-
 }
